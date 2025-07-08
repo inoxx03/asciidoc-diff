b4ec97c5da93fd3dca501841c67bfe748603fe83
Stefan Sitani <stefan.sitani@gmail.com>
2025-07-08 22:58:47 +0200

    Added comments, licensing info, and a list of lower-prio enhancements

--- diff.js
+++ diff.js
@@ -1,62 +1,112 @@
-#!/usr/bin/env node
+/*
+    AsciiDoc Diff v1.0.0
 
-// Import Node.js standard modules using ES module syntax
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import htmldiff from 'htmldiff-js';
-//import HtmlDiff from 'htmldiff-js';
+    Copyright 2020-2025 Alexander Schwartz
+    (alexander.schwartz@gmx.net)
 
-// Required for __dirname replacement in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-// Get CLI arguments (after the first two: node + script path)
-const [fileNew, fileOld, fileOut] = process.argv.slice(2);
+        http://www.apache.org/licenses/LICENSE-2.0
 
-// Validate input
-if (!fileNew || !fileOld || !fileOut) {
-console.error("Usage: asciidoc-diff <new.html> <old.html> <output.html>");
-process.exit(1);
-}
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 
-// Resolve absolute paths
-const filepathNew = path.resolve(process.cwd(), fileNew);
-const filepathOld = path.resolve(process.cwd(), fileOld);
-const outputPath = path.resolve(process.cwd(), fileOut);
+    Original script by Alexander Schwartz (alexander.schwartz@gmx.net) 
+    Subsequent modifications by Stefan Sitani (stefan.sitani@gmail.com)
 
-// DEBUG
-console.log("Working directory:", process.cwd());
-console.log("New file path:", filepathNew);
-console.log("Old file path:", filepathOld);
-console.log("Output file path:", outputPath);
+*/
 
-// Read HTML files
-const newHtml = fs.readFileSync(filepathNew, 'utf8');
-const oldHtml = fs.readFileSync(filepathOld, 'utf8');
+/*
+    Lower priority fixes:
+    TODO: Print usage information to STDOUT and exit when no input or output file arguments are specified.
+    TODO: Add a means of scpefying a custom path for the input and output files.
+    TODO: Gracefully handle errors if one or both input file are not specified.
+    TODO: Use a default output file name, if none is provided.
+    TODO: Maybe also use the default as fallback if specified output file already exists.
+    TODO: Add a means of specifying a custom CSS style sheet for the diff output HTML.
+    TODO: Gracefully handle invalid or empty HTML files when passed as input. 
+*/
 
-// Generate diff
+const HtmlDiff = require('htmldiff-js').default;
+const fs = require('fs');
+const { allowedNodeEnvironmentFlags } = require('process');
+path = require('path');
+
+// List processed arguments starting at index 2, trims the "HtmlDiff" and "fs" constants
+const arg = process.argv.slice(2);
+// Get the path to the current working directory.
+// Used as the output path.
+const workdir = process.cwd();
+
+// DEBUG: Print current working directory
+console.log("Current working directory: ",
+    process.cwd());
+
+// New HTML
+// Name of input file 1
+arg[0] = String(arg[0]);
+// Old HTML
+// Name of input file 2
+arg[1] = String(arg[1]);
+// Name of the output file
+arg[2] = String(arg[2]);
+
+// append workdir path to input file 1
+var filepathNew = path.join(workdir, arg[0]);
+// DEBUG: print path to NEW input file
+console.log('Path to new HTML file: ', filepathNew);
+
+// append workdir path to input file 2 (OLD)
+var filepathOld = path.join(workdir, arg[1]);
+// DEBUG: print path to OLD input file
+console.log('Path to old HTML file: ', filepathOld);
+
+
+// specify output file name
+var outputPath = path.join(workdir, arg[2]);
+console.log('Path to output file:', outputPath);
+
+var newHtml = fs.readFileSync(filepathNew, 'utf8');
+var oldHtml = fs.readFileSync(filepathOld, 'utf8');
+
+// DEBUG: print a message for the log
 console.log("Comparing files and generating diff...");
-//let diffHtml = HtmlDiff.execute(oldHtml, newHtml);
-//let diffHtml = HtmlDiff(oldHtml, newHtml);
-let diffHtml = htmldiff.default.execute(oldHtml, newHtml);
+
+// Generate the diff
+var diffHtml = HtmlDiff.execute(oldHtml, newHtml);
+
 console.log("Done!");
 
-// Inject custom styling for insertions and deletions
-diffHtml = diffHtml.replace("<head>", `<head><style>
+/*
+    Append CSS classes used for highlighting additions, deletions, 
+    and for highlighting modified blocks, to the inline CCS from 
+    the input HTML files.
+    Not tested with 2 input files with different inline CSS, yet. 
+*/
+diffHtml = diffHtml.replace("<head>",`<head><style>
 del {
-text-decoration: none;
-background-color: #fdb8c0;
+    text-decoration: none;
+    background-color: #fdb8c0;
 }
-ins {
-text-decoration: none;
-background-color: #acf2bd;
-}
-ins.mod {
-background-color: #fafaa9;
-}
-</style>`);
 
-// Write to output
-fs.writeFileSync(outputPath, diffHtml, 'utf8');
-console.log(`HTML diff written to: ${outputPath}`);
+ins {
+    text-decoration: none;
+    background-color: #acf2bd;
+}
+
+/* just formatting changes */
+ins.mod {
+    background-color: #fafaa9;
+}
+</style>`)
+
+
+fs.writeFile(arg[2], diffHtml, function (err) {
+    if (err) throw err;
+    console.log('HTML diff generated in', path.join(workdir, arg[2]));
+});

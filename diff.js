9539b837df6f13954d7bcda5e0ffc4266b9d5433
Stefan Sitani <stefan.sitani@codasip.com>
2025-07-28 10:51:27 +0200

    initilaize 'process' as a variable

--- diff.js
+++ diff.js
@@ -9,8 +9,9 @@ import htmldiff from 'htmldiff-js';
 // Required for __dirname replacement in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+var process
 
-// Process CLI arguments  For input and output files
+// Process CLI arguments for input and output files
 // Trim the first 2 default positional arguments that provide `node` and script path
 const [fileNew, fileOld, fileOut] = process.argv.slice(2);
 
